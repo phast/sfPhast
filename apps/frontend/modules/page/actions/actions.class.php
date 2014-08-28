fef5e52e31577a868f19a5723591e51181d4c19e
Pavel B <mrpro@mail.ru>
2014-08-28 10:38:58 +0700

    frontend page module

---
+++ apps/frontend/modules/page/actions/actions.class.php
@@ -0,0 +1,17 @@
+<?php
+	
+ 
+class pageActions extends sfActions
+{
+  
+	public function executeStatic(sfPhastRequest $request){
+	}
+
+	public function executeIndex(sfPhastRequest $request){
+	}
+
+}
+
+
+
+
