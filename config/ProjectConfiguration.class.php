87dd3c938c3fd0309a09949a3bf07c2a022e4977
Pavel B <mrpro@mail.ru>
2014-10-20 11:33:09 +0700

    composer autoloader backend module

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,7 +1,13 @@
 <?php
 
 require_once dirname(__FILE__) . '/../lib/vendor/symfony/symfony1/lib/autoload/sfCoreAutoload.class.php';
+require_once __DIR__ . '/../lib/vendor/composer/ClassLoader.php';
+
 sfCoreAutoload::register();
+$loader = new \Composer\Autoload\ClassLoader();
+$map = require __DIR__ . '/../lib/vendor/composer/autoload_namespaces.php';
+foreach ($map as $namespace => $path) $loader->set($namespace, $path);
+$loader->register(true);
 
 
 class ProjectConfiguration extends sfProjectConfiguration
