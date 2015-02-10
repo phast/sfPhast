287ac236156c3e4b927ad1f393e344256af12689
Pavel B <p.bondarovich@gmail.com>
2015-02-10 18:26:44 +0600

    project config

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -17,6 +17,10 @@ class ProjectConfiguration extends sfProjectConfiguration
         date_default_timezone_set('Etc/GMT-4');
         mb_internal_encoding('UTF-8');
 
+        sfConfig::set('sf_propel_path', sfConfig::get('sf_lib_dir') . '/vendor/propel/propel1');
+        sfConfig::set('sf_twig_lib_dir', sfConfig::get('sf_lib_dir') . '/vendor/twig/twig/lib');
+        sfConfig::set('sf_phing_path', sfConfig::get('sf_lib_dir') . '/vendor/phing/phing');
+
         $this->enablePlugins('sfPropelORMPlugin', 'sfPhastPlugin', 'sfImageTransformPlugin');
     }
 
